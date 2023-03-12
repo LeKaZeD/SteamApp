382b1a7e0f799ba7d07ea917945710320b82cdb6
LeKaZeD <alex.konzy@gmail.com>
2023-03-12 18:12:18 +0100

    feat: user et userEntity

---
+++ lib/domain/entities/userEntity.dart
@@ -0,0 +1,12 @@
+class userEntiy {
+  final String email;
+  final String mdp;
+  final List<dynamic> wish;
+  final List<dynamic> like;
+
+  userEntiy(
+      {required this.email,
+      required this.mdp,
+      required this.wish,
+      required this.like});
+}
