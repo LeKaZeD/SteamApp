9059af443ab2063df401c99063e68ce8bdb75672
Alexandre Konzelmann <alex.konzy@gmail.com>
2023-03-19 21:36:17 +0100

    appnavigatorBloc

---
+++ lib/UI/MyApp/bloc/appNavigatorBloc.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class AppNavigatorEvent {}
+
+class AppNavigatorState {
+
+}
+
+class AppNavigatorBloc extends Bloc<AppNavigatorEvent, AppNavigatorState> {
+  AppNavigatorBloc() : super() {
+  }
+
+}
+
